1f60f4f29107d3879f3d46a0ad57f5c8e9d83ad8
cp-jwu130 <43765758+cp-jwu130@users.noreply.github.com>
2018-11-03 19:57:02 -0700

    Added files with basic layout and a Test class. -JW

---
+++ src/logic/Test.java
@@ -0,0 +1,8 @@
+package logic;
+
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Hello Sir...\n" +
+                           "I hope your day has been pleasant so far.");
+    }
+}
